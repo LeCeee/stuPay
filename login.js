310cf6ae5c5b5201f19043fe5589164785c8acb0
LeCeee <aakarshitatiwari@gmail.com>
2018-10-05 04:39:36 +0530

    ui changes

--- login.js
+++ login.js
@@ -2,9 +2,23 @@
 document.getElementById("superBut").onclick = function() {showForm()};
 
 function showForm() {
-document.querySelector('.loginForm').style.display = 'block';
+// document.querySelector('.form-wrapper').style.display = 'block';
+
+ document.querySelector('#libBut').style.display = 'none';
+ location.href = "/home/cee/Documents/webD/stupay/superAd.html";
+
+};
+
+document.getElementById("libBut").onclick = function() {showLibForm()};
+
+function showLibForm() {
+// document.querySelector('.form-wrapper').style.display = 'block';
+ document.querySelector('#superBut').style.display = 'none';
+ location.href = "/home/cee/Documents/webD/stupay/lib.html";
 };
 
 
 
-
+document.getElementById("myButton").onclick = function () {
+   
+};
