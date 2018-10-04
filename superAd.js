f486938081aa382cae4f2b5db097d1a6fa108951
LeCeee <aakarshitatiwari@gmail.com>
2018-10-05 02:51:36 +0530

    add money + library update

--- superAd.js
+++ superAd.js
@@ -1,7 +1,10 @@
 document.getElementById("createBut").onclick = function() {showCreateForm()};
 
 function showCreateForm() {
+document.querySelector('.add-money-container').style.display = 'none';
+    
 document.querySelector('.create-action-contaier').style.display = 'block';
+
 };
 
 // var e = document.getElementById("account-type");
@@ -48,14 +51,14 @@ function submitStudentForm(e){
     var rollNo = getInput('rollnum')
 
     saveStudent(name,id,rollNo);
-    // document.querySelector('.alert').style.display = 'block';
+    document.querySelector('.alert').style.display = 'block';
 
-    // setTimeout(function() {
-    // document.querySelector('.alert').style.display = 'none';
+    setTimeout(function() {
+    document.querySelector('.alert').style.display = 'none';
 
-    // },3000);
+    },3000);
 
-    //document.getElementById('Create-Action-student').reset();
+    document.getElementById('Create-Action-student').reset();
 
     console.log(name);
 }
@@ -70,43 +73,91 @@ function saveStudent(name,id,rollNo) {
         name:name,
         id:id,
         rollNo:rollNo,
-        currentBalance:0
+        currentBalance: 0
     });
 }
 
-// var studentRef = firebase.database().ref('NonStudent');
+ var studentRef = firebase.database().ref('NonStudent');
 
-// document.getElementById('vendor-form').addEventListener('submit',submitVendorForm);
+document.getElementById('vendor-form').addEventListener('submit',submitVendorForm);
 
-// function submitVendorForm(e){
-//     e.preventDefault();
-//     var name = getInput('name');
-//     var id = getInput('ID');
-//     var rollNo = getInput('rollnum')
+function submitVendorForm(e){
+    e.preventDefault();
+    var name = getInput('name');
+    var id = getInput('ID');
+    // var rollNo = getInput('rollnum')
 
-//     saveStudent(name,id,rollNo);
-//     // document.querySelector('.alert').style.display = 'block';
+    saveVendor(name,id);
+    document.querySelector('.alert').style.display = 'block';
 
-//     // setTimeout(function() {
-//     // document.querySelector('.alert').style.display = 'none';
+    setTimeout(function() {
+    document.querySelector('.alert').style.display = 'none';
 
-//     // },3000);
+    },3000);
 
-//     //document.getElementById('Create-Action-student').reset();
+    document.getElementByClassName('Create-Action-vendor').reset();
 
-//     console.log(name);
-// }
+    console.log(name);
+}
 
 
-// function getInput(id){
-//     return   document.getElementById(id).value;
-// }
+function saveVendor(name,id) {
+    console.log(alert);
+}
 
-// function saveStudent(name,id,rollNo) {
-//     var newStudentRef = studentRef.push({
-//         name:name,
-//         id:id,
-//         rollNo:rollNo,
-//         currentBalance:0
+
+
+
+document.getElementById("updateBut").onclick = function() {showUpdateForm()};
+
+function showUpdateForm() {
+document.querySelector('.create-action-contaier').style.display = 'none';
+
+document.querySelector('.add-money-container').style.display = 'block';
+};
+
+document.getElementById('add-money-id').addEventListener('submit',submitMoneyForm);
+
+
+var studentRef = firebase.database().ref('Student');
+
+
+function submitMoneyForm (e) {
+    e.preventDefault();
+    var amount = getInput('amount');
+    var id = getInput('stID');
+    // var rollNo = getInput('rollnum')
+
+    updateMoney(id,amount);
+    // document.querySelector('.alert').style.display = 'block';
+
+    // setTimeout(function() {
+    // document.querySelector('.alert').style.display = 'none';
+
+    // },3000);
+
+    // document.getElementByClassName('Create-Action-vendor').reset();
+}
+
+function updateMoney(id,amount) {
+    firebase.database().ref('Student/' + id).update({
+        currentBalance: amount ,
+        name: "cee",
+        rollNo: 12301012016
+
+
+       });
+    // var curBal = studentRef.child("id").child('currentBalance');
+
+    // curBal.transaction(function(currentBalance,amount) {
+    // return parseInt(currentBalance)+ amount;})
+}
+
+// function writeUserData(userId, name, email, imageUrl) {
+//     firebase.database().ref('users/' + userId).set({
+//       username: name,
+//       email: email,
+//       profile_picture : imageUrl
 //     });
-// }
+//   }
+
