4892a907e04d8e224c06b10b6ebcc64a41111d77
LeCeee <aakarshitatiwari@gmail.com>
2018-10-05 00:10:55 +0530

    login+superadmin page

---
+++ login.js
@@ -0,0 +1,10 @@
+
+document.getElementById("superBut").onclick = function() {showForm()};
+
+function showForm() {
+document.querySelector('.loginForm').style.display = 'block';
+};
+
+
+
+
---
+++ superAd.js
@@ -0,0 +1,74 @@
+document.getElementById("createBut").onclick = function() {showCreateForm()};
+
+function showCreateForm() {
+document.querySelector('.create-action-contaier').style.display = 'block';
+};
+
+// var e = document.getElementById("account-type");
+// var value = e.options[0].value;
+// var type = e.options[0].text;
+// e.onclick = function(){heck()};
+// function heck() {
+//     console.log('hello')
+// }
+// console.log(type.text);
+
+ document.getElementById("student").onclick = function() {displayStudentForm()};
+ document.getElementById("vendor").onclick = function() {displayVendorForm()};
+
+function displayStudentForm() {
+    document.querySelector('.Create-Action-vendor').style.display = 'none';        
+    document.querySelector('.Create-Action-student').style.display = 'block';
+    
+     
+};
+function displayVendorForm() {
+    document.querySelector('.Create-Action-student').style.display = 'none';
+document.querySelector('.Create-Action-vendor').style.display = 'block';
+
+};
+var config = {
+    apiKey: "AIzaSyCWbg7gm39tWGy5spIFkmZuhMNobLHhPtY",
+    authDomain: "stupay-44266.firebaseapp.com",
+    databaseURL: "https://stupay-44266.firebaseio.com",
+    projectId: "stupay-44266",
+    storageBucket: "stupay-44266.appspot.com",
+    messagingSenderId: "707878873067"
+  };
+  firebase.initializeApp(config);
+ 
+
+var studentRef = firebase.database().ref('Student')
+document.getElementById('Create-Action-student').addEventListener('submit',submitStudentForm);
+function submitForm(e){
+    e.preventDefault();
+    var name = getInput('name');
+    var id = getInput('ID');
+    var rollNo = getInput('rollnum')
+
+    saveStudent(name,id,rollNo);
+    // document.querySelector('.alert').style.display = 'block';
+
+    // setTimeout(function() {
+    // document.querySelector('.alert').style.display = 'none';
+
+    // },3000);
+
+    document.getElementById('Create-Action-student').reset();
+
+    console.log(name);
+}
+
+
+function getInput(id){
+    return   document.getElementById(id).value;
+}
+
+function saveMessages(name,id,rollNo) {
+    var newStudentRef = studentRef.push({
+        name:name,
+        id:id,
+        rollNo:rollNo,
+        currentBalance:0
+    });
+}
